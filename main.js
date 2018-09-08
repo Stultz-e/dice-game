9a34071632ed924daaeacea876bc6191c219bf62
cstultz87 <cstultz@oswego.edu>
2018-09-08 12:59:48 -0500

    f

--- main.js
+++ main.js
@@ -413,3 +413,49 @@ var john = {
         } 
     }
 }
+
+
+var mark = {
+    fullName: 'Mark Miller',
+    bills: [77, 5, 110, 45, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+            var percentage; 
+            var bill = this.bills[i];
+
+            if (bill < 100 ) {
+                percentage = .2;
+            } else if (bill >= 100 && bill < 300) {
+                percentage = .1;
+            } else {
+                percentage = .25;
+            }
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        } 
+    }
+}
+
+function calcAverage(tips) {
+    var sum = 0;
+    for (var i = 0; i < tips.length; i++) {
+        sum = sum + tips[i];
+    }
+    return sum / tips.length;
+}
+
+// Do the calculations
+john.calcTips();
+mark.calcTips();
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if (john.average > mark.average) {
+    console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+} else if (mark.average > john.average) {
+    console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
+}
